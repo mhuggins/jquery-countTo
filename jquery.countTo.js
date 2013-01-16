1a026545634317481e7f9c7e993d0e88af8b4838
Matt Huggins <matt.huggins@gmail.com>
2013-01-15 21:39:00 -0500

    Allow for DOM data attributes and custom formatting when rendering

--- jquery.countTo.js
+++ jquery.countTo.js
@@ -1,46 +1,70 @@
-(function($) {
-	$.fn.countTo = function(options) {
+(function ($) {
+	$.fn.countTo = function (options) {
 		// merge the default plugin settings with the custom options
 		options = $.extend({}, $.fn.countTo.defaults, options || {});
 		
-		// how many times to update the value, and how much to increment the value on each update
-		var loops = Math.ceil(options.speed / options.refreshInterval),
-			increment = (options.to - options.from) / loops;
-		
-		return $(this).each(function() {
+		return $(this).each(function () {
+			// set options for current element
+			var settings = $.extend({}, options, {
+				from:            parseInt($(this).attr('data-from') || options.from, 10),
+				to:              parseInt($(this).attr('data-to') || options.to, 10),
+				speed:           parseInt($(this).attr('data-speed') || options.speed, 10),
+				refreshInterval: parseInt($(this).attr('data-refresh-interval') || options.refreshInterval, 10),
+				decimals:        parseInt($(this).attr('data-decimals') || options.decimals, 10)
+			});
+			
+			// how many times to update the value, and how much to increment the value on each update
+			var loops = Math.ceil(settings.speed / settings.refreshInterval),
+				increment = (settings.to - settings.from) / loops;
+			
+			// references & variables that will change with each update
 			var self = this,
 				loopCount = 0,
-				value = options.from,
-				interval = setInterval(updateTimer, options.refreshInterval);
+				value = settings.from,
+				interval = setInterval(updateTimer, settings.refreshInterval);
+			
+			// initialize the element with the starting value
+			render(value);
 			
 			function updateTimer() {
 				value += increment;
 				loopCount++;
-				$(self).html(value.toFixed(options.decimals));
 				
-				if (typeof(options.onUpdate) == 'function') {
-					options.onUpdate.call(self, value);
+				render(value);
+				
+				if (typeof(settings.onUpdate) == 'function') {
+					settings.onUpdate.call(self, value);
 				}
 				
 				if (loopCount >= loops) {
 					clearInterval(interval);
-					value = options.to;
+					value = settings.to;
 					
-					if (typeof(options.onComplete) == 'function') {
-						options.onComplete.call(self, value);
+					if (typeof(settings.onComplete) == 'function') {
+						settings.onComplete.call(self, value);
 					}
 				}
+			}
+			
+			function render(value) {
+				var formattedValue = settings.formatter.call(self, value, settings);
+				$(self).html(formattedValue);
 			}
 		});
 	};
 	
 	$.fn.countTo.defaults = {
-		from: 0,  // the number the element should start at
-		to: 100,  // the number the element should end at
-		speed: 1000,  // how long it should take to count between the target numbers
+		from: 0,               // the number the element should start at
+		to: 0,                 // the number the element should end at
+		speed: 1000,           // how long it should take to count between the target numbers
 		refreshInterval: 100,  // how often the element should be updated
-		decimals: 0,  // the number of decimal places to show
-		onUpdate: null,  // callback method for every time the element is updated,
-		onComplete: null  // callback method for when the element finishes updating
+		decimals: 0,           // the number of decimal places to show
+		formatter: formatter,  // handler for formatting the value before rendering
+		onUpdate: null,        // callback method for every time the element is updated
+		onComplete: null       // callback method for when the element finishes updating
 	};
+	
+	function formatter(value, settings) {
+		return value.toFixed(settings.decimals);
+	}
 }(jQuery));
