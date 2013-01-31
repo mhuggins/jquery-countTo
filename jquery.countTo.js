b6c9a2164a79d3c82af643638e3514242c8ca1cd
Matt Huggins <matt.huggins@gmail.com>
2013-01-30 20:08:57 -0500

    Remove extraneous parseFloat arguments, and add float count example

--- jquery.countTo.js
+++ jquery.countTo.js
@@ -6,8 +6,8 @@
 		return $(this).each(function () {
 			// set options for current element
 			var settings = $.extend({}, options, {
-				from:            parseFloat($(this).attr('data-from') || options.from, 10),
-				to:              parseFloat($(this).attr('data-to') || options.to, 10),
+				from:            parseFloat($(this).attr('data-from') || options.from),
+				to:              parseFloat($(this).attr('data-to') || options.to),
 				speed:           parseInt($(this).attr('data-speed') || options.speed, 10),
 				refreshInterval: parseInt($(this).attr('data-refresh-interval') || options.refreshInterval, 10),
 				decimals:        parseInt($(this).attr('data-decimals') || options.decimals, 10)
